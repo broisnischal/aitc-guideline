46aabcf512bc3e4724ae291ed4a694d0a2b738d3
neeswebservice <neeswebservice@gmail.com>
2024-01-24 14:00:37 +0545

    feat:added more docs

--- theme.config.tsx
+++ theme.config.tsx
@@ -11,7 +11,30 @@ const config: DocsThemeConfig = {
   },
   docsRepositoryBase: "https://github.com/broisnischal/aitc-guideline",
   footer: {
-    text: "Nextra Docs Template",
+    text: "Copyright © 2022 AITC International. All rights reserved.",
+  },
+  banner: {
+    text: "We are under construction!",
+  },
+  notFound: {
+    labels: "Page Not Found",
+    content: "Sorry, the page you visited does not exist.",
+  },
+  sidebar: {
+    titleComponent: (props) => (
+      <h1 className="text-2xl font-bold">{props.title}</h1>
+    ),
+    autoCollapse: false,
+    toggleButton: true,
+  },
+  toc: {
+    title: "Table of Contents",
+    float: true,
+  },
+  nextThemes: {},
+  primaryHue: {
+    dark: 10,
+    light: 30,
   },
 };
 
